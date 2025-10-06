c275ec4031197deae7ac76e5f81097a16d8c59a1
DaviRdaSilva <davi.rsilva@sptech.school>
2025-10-06 16:26:19 -0300

    Correção - Ajuste no tipo do campo dentro da classe Equipe

--- src/main/kotlin/com/timesync/model/Equipe.kt
+++ src/main/kotlin/com/timesync/model/Equipe.kt
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
 import java.util.UUID
 
@@ -15,7 +17,8 @@ class Equipe {
     @Id @GeneratedValue(strategy = GenerationType.UUID)
     val idEquipe: UUID? = null
 
-    @Column
-    val usuario_matricula: Int? = null
+//    @field:ManyToOne
+//    @JoinColumn(name = "usuarios_matricula")
+//    val gestor: Usuarios? = null
 
 }

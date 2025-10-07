13e2ebb3a1a44e8cb363f8bec829d7a1b87ab388
DaviRdaSilva <davi.rsilva@sptech.school>
2025-10-07 18:23:36 -0300

    Correção - Adição do @GetMapping na função de Busca

--- src/main/kotlin/com/timesync/controller/AssocUsuarioProjetosController.kt
+++ src/main/kotlin/com/timesync/controller/AssocUsuarioProjetosController.kt
@@ -2,6 +2,7 @@ package com.timesync.controller
 
 import com.timesync.model.AssocUsuarioProjetos
 import com.timesync.service.AssocUsuarioProjetosService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -11,6 +12,7 @@ class AssocUsuarioProjetosController (
     private val assocUsuarioProjetosService: AssocUsuarioProjetosService
 ) {
 
+    @GetMapping
     fun listarTodos(): List<AssocUsuarioProjetos> = assocUsuarioProjetosService.listarTodos()
 
 }
